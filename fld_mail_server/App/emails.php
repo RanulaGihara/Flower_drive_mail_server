ffd9eb6c85825ddb97e7821e3e63fe9d7f667632
ranula97 <52966123+RanulaGihara@users.noreply.github.com>
2023-07-31 18:11:21 +0530

    Update emails.php

--- fld_mail_server/App/emails.php
+++ fld_mail_server/App/emails.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class FlowerDrive extends Model
+class email extends Model
 {
     public $timestamps = true;
     use SoftDeletes;
@@ -14,57 +14,17 @@ class FlowerDrive extends Model
     protected $guarded = ['fld_id'];
 
     public function flowerDriveSite(){
-        return $this->hasMany('App\FlowerDriveSite','fld_id','fld_id');
-    }
-
-    public function lines(){
-        return $this->hasMany('App\FlowerDriveLine', 'fld_id');
-    }
-
-    public function specialOccassion(){
-
-        return $this->belongsTo('App\SpecialOccassion','special_oc_id','occ_id');
-    }
-
-    public function occasion()
-    {
-        return $this->hasOne('App\SpecialOccassion', 'occ_id', 'special_oc_id');
+        return $this->hasMany('App\site','id','s_id');
     }
 
     public function items()
     {
-        return $this->hasMany('App\FlowerDriveItem', 'fld_id', 'fld_id');
+        return $this->hasMany('App\item', 'id', 'id');
     }
 
-    public function fldContacts(){
-        return $this->hasMany('App\FlowerDriveContact','fld_id','fld_id');
-    }
 
-    public function consumerOrder()
+    public function order()
     {
-        return $this->hasMany('App\ConsumerOrder', 'fld_id', 'fld_id');
+        return $this->hasMany('App\ConsumerOrder', 'id', 'c_id');
     }
-
-    //To get User tenanat id
-
-    public function getUserTenant()
-    {
-        return $this->hasOne('App\User', 'id', 'created_by');
-    }
-
-    public function getSelectedContactList()
-    {
-        return $this->hasOne('App\ContactList', 'id', 'contact_list_id');
-    }
-
-    public function timezone()
-    {
-        return $this->hasOne('App\TimeZone', 'id', 'timezone');
-    }
-
-    public function sectionOrArea()
-    {
-        return $this->hasMany('App\FldSections', 'fld_id', 'fld_id');
-    }
-
 }
