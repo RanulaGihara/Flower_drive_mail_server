c2b27eb2dac5a90ca49349ccdf070d53f1d82602
ranula97 <52966123+RanulaGihara@users.noreply.github.com>
2023-07-31 18:09:40 +0530

    Update contact.php

--- fld_mail_server/App/contact.php
+++ fld_mail_server/App/contact.php
@@ -4,36 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class FlowerDriveContact extends Model
+class contact extends Model
 {
     public $timestamps = false;
 
-    protected $table = 'FLD_CONTACT_TB';
+    protected $table = 'CONTACT_TB';
     protected  $primaryKey = 'id';
     protected $keyType = 'char';
-//        protected  $guarded = ['id'];
-    public $incrementing = false;
 
-    protected  $fillable = ['id','fld_id','contact_id','created_by','created_at','updated_by','updated_at','deleted_at'];
+    protected  $fillable = ['id','contact_id','created_by','created_at','updated_by','updated_at','deleted_at'];
 
-    public function flowerDrive(){
-        return $this->belongsTo('App\FlowerDrive','fld_id','fld_id');
-    }
-
-    public function person() {
-        return $this->hasOne('App\Person', 'id', 'contact_id');
+    public function mail(){
+        return $this->belongsTo('App\Mails','id','e_id');
     }
 
     public function contactSite() {
-        return $this->hasMany('App\FlowerDriveContactSite', 'fld_contact_id', 'id');
+        return $this->hasMany('App\contactSite', 'contact_id', 'id');
     }
-    public function fldBmContactSite() {
-        return $this->hasOne('App\FlowerDriveByondMarketContact', 'fld_contact_id', 'id');
-    }
-
-    public function territory()
-    {
-        return $this->belongsTo('App\Territory', 'terr_id', 'terr_id');
-    }
+   
 }
 
